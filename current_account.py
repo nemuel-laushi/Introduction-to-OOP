193f5e7c28f1fa42dd6ecbe9c97d45397bd1832f
nemuel-laushi <151081201+nemuel-laushi@users.noreply.github.com>
2024-06-07 00:52:04 +0100

    Update current_account.py

--- current_account.py
+++ current_account.py
@@ -1,14 +1,29 @@
 from account import Account
+
 class CurrentAccount(Account):
     
     def __init__(self):
         super().__init__()
+
+
         
 currentAccount = CurrentAccount()
 
-currentAccount.deposit(2000)
-        
-        
-        
+opt = input('IF YOU WANT TO DEPOSIT INPUT a lower case"d", AND IF YOU WANT TO WITHDRAW INPUT a lower case "w": ')
+
+if opt == 'w': 
+
+        amw = float(input('Enter desired  withdrawal amount: '))
+        currentAccount.withdraw(amw)
     
-            
+        print('AMOUNT MUST BE NUMERIC')
+    
+if opt == 'd':
+    
+        amd = float(input('Enter deposit amount: '))
+           
+    
+
+        currentAccount.deposit(amd)
+    
+        
